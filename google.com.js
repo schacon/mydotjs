3bc2eeff7c72330f72962d10b00d8e77b118a0a1
Scott Chacon <schacon@gmail.com>
2011-07-01 11:41:09 -0700

    new google calendar fix

---
+++ google.com.js
@@ -0,0 +1,6 @@
+$('.domainlogo img').hide()
+$('#mb1').hide()
+$('#vr-proto-header').hide()
+$('#vr-proto-nav').css('padding', '0')
+$('#nav').css('margin', '0').css('width', '130px')
+$('#mainbody').css('margin-left', '140px')
